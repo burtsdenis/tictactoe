07f2e9730eb919f24056f6224cf7baaec1ba098c
denis.burtsev <denis.burtsev@thinfilmnfc.com>
2020-04-13 23:16:24 +0300

    some refactoring again

--- src/TicTacToe/Main.java
+++ src/TicTacToe/Main.java
@@ -8,40 +8,50 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        int x = -1;
+        int y = -1;
+
         System.out.print("Enter name for player №" + Player.playersCount + ": ");
-        Player player1 = new Player(reader.readLine(),"X");
+        Player player1 = new Player(reader.readLine(), "X");
         System.out.print("Enter name for player №" + Player.playersCount + ": ");
-        Player player2 = new Player(reader.readLine(),"O");
+        Player player2 = new Player(reader.readLine(), "O");
 
         TicTacToe game1 = new TicTacToe(3);
-        game1.initializeGame(player1,player2);
+        game1.initializeGame(player1, player2);
 
         while (true) {
-            System.out.print("Enter x: ");
-
-            int x = checkInt(reader.readLine());
-            System.out.print("Enter y: ");
-            int y = checkInt(reader.readLine());
+            try {
+                if (x < 0) {
+                    System.out.print("Enter x: ");
+                    x = Integer.parseInt(reader.readLine());
+                }
+                if (y < 0) {
+                    System.out.print("Enter y: ");
+                    y = Integer.parseInt(reader.readLine());
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Your input is incorrect, value must Integer!");
+                continue;
+            }
 
             if (!game1.checkInput(x, y)) continue;
-            if (!game1.turnAllowed(x, y)) continue;
+            if (!game1.turnAllowed(x, y))  { x = -1; y = -1; continue; }
 
             TicTacToe.turnNumber++;
             if (TicTacToe.turnNumber % 2 != 0) {
-                if (game1.makeATurn(x,y,player1)) { System.out.println(player1.name + " wins!"); break; }
+                if (game1.makeATurn(x, y, player1)) {
+                    System.out.println(player1.name + " wins!");
+                    break;
+                }
             } else if (TicTacToe.turnNumber % 2 == 0) {
-                if (game1.makeATurn(x,y,player2)) { System.out.println(player2.name + " wins!"); break; }
+                if (game1.makeATurn(x, y, player2)) {
+                    System.out.println(player2.name + " wins!");
+                    break;
+                }
             }
+            x = -1;
+            y = -1;
         }
-    }
-
-    public static int checkInt(String digit) {
-        try {
-            return Integer.parseInt(digit);
-        } catch (NumberFormatException e) {
-        }
-        System.out.println("Your input is incorrect, values must Integer!");
-        return 0;
     }
 
 }
