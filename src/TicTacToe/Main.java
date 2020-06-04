c0eae39bb032eb75c469d5e4d010a2d56ab6e88a
denis.burtsev <denis.burtsev@thinfilmnfc.com>
2020-06-04 19:17:03 +0300

    fix the typo

--- src/TicTacToe/Main.java
+++ src/TicTacToe/Main.java
@@ -2,7 +2,7 @@ package TicTacToe;
 
 import java.util.Scanner;
 
-import static ru.geekbrains.lesson4.GameProcess.initializeGame;
+import static TicTacToe.TicTacToe.initializeGame;
 
 public class Main {
     public static final Scanner scan = new Scanner(System.in);
